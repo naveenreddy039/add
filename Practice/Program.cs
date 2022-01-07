ae2f6d19e8d2e45df9f7bfbb92358b4e5eb9bf19
naveen <naveenreddy.k07@gmail.com>
2022-01-07 19:35:25 +0530

    add Files

---
+++ Practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //UC1
+            int a = 10;
+            int b = 5;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
